2ccd4e109a91e810f6b3b4ffaf18b2a652d5ec5a
fingligroup <maksim@fingli.ru>
2020-10-28 23:59:53 +0300

    Update CORE_KrukovoVedomostiRu_Parser.php
    approved

--- components/parser/news/CORE_KrukovoVedomostiRu_Parser.php
+++ components/parser/news/CORE_KrukovoVedomostiRu_Parser.php
@@ -138,7 +138,7 @@ class CORE_KrukovoVedomostiRu_Parser extends ParserCore implements ParserInterfa
                 // игнорируемые css-селекторы
                 // (можно через запятую)
                 // (опционально)
-                'ignore-selectors'    => '#ap_news_detail_page_tags, .ap_news_detail_page_txt a:last-child, a[href^="/print.php?"]',
+                'ignore-selectors'    => 'h1, span.news-date-time, #ap_news_detail_page_tags, .ap_news_detail_page_txt a:last-child, a[href^="/print.php?"], .detail_picture_caption_div',
             ]
         ];
 
