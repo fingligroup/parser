65f122116daf32f773fd43fa05d732e14e15460c
Roman Goncharenya <goncharenya@gmail.com>
2020-11-02 20:57:00 +0300

    part 3 approved by rmn

--- components/parser/news/CORE_KhabarovskMd_Parser.php
+++ components/parser/news/CORE_KhabarovskMd_Parser.php
@@ -17,7 +17,7 @@ namespace app\components\parser\news;
 use fingli\ParserCore\ParserCore;
 use app\components\parser\ParserInterface;
 
-// part 3 approved by
+// part 3 approved by rmn
 class CORE_KhabarovskMd_Parser extends ParserCore implements ParserInterface
 {
     const USER_ID = 2;
@@ -30,7 +30,7 @@ class CORE_KhabarovskMd_Parser extends ParserCore implements ParserInterface
     // 1 - включен
     // 2 - включен (очень подробный режим)
     // 3 - режим "зануда"
-    protected const DEBUG = 1;
+    protected const DEBUG = 0;
 
     public function __construct()
     {
@@ -51,7 +51,7 @@ class CORE_KhabarovskMd_Parser extends ParserCore implements ParserInterface
             'site'    => [
                 // протокол и домен
                 // (обязательный)
-                'url'         => '',
+                'url'         => 'https://khabarovsk.md/',
 
                 // использовать юзер-агенты в http запросах.
                 // (опционально)
@@ -63,7 +63,7 @@ class CORE_KhabarovskMd_Parser extends ParserCore implements ParserInterface
                 // узнать UTC и прописать его в формате +XX00
                 // Например, Москва: '+0300', Владивосток: '+1000'
                 // (опционально)
-                'time_zone'   => '+0300',
+                'time_zone'   => '+1000',
 
                 // формат даты для HTML витрины и карточки
                 // (см. https://www.php.net/manual/ru/datetime.format.php)
@@ -87,71 +87,33 @@ class CORE_KhabarovskMd_Parser extends ParserCore implements ParserInterface
             'rss'     => [
                 // относительный URL где находится RSS
                 // (обязательный)
-                'url'                 => '',
+                'url'           => '/rss.xml',
 
                 // css селектор для элемента витрины (желательно от корня)
                 // (обязательный)
-                'element'             => 'rss > channel > item',
+                'element'       => 'rss > channel > item',
 
                 // ** дальнейшие css-селекторы указываются относительно element
 
                 // css селектор для названия элемента
                 // (обязательный)
-                'element-title'       => 'title',
+                'element-title' => 'title',
 
                 // css селектор для ссылки
                 // (обязательный)
-                'element-link'        => 'link',
+                'element-link'  => 'link',
 
                 // css селектор для описания элемента
                 // (опционально)
-                'element-description' => 'description',
+                //                'element-description' => 'description',
 
                 // css селектор для картинки элемента
                 // (опционально)
-                'element-image'       => 'enclosure[url]',
+                'element-image' => 'enclosure[url]',
 
                 // css селектор для даты элемента
                 // (опционально)
-                'element-date'        => 'pubDate',
-            ],
-
-            // настройки витрины (режим HTML)
-            // !!! заполняется, только при отсутствии витрины RSS !!!
-            'list'    => [
-                // URL где находится витрина
-                // (обязательный)
-                'url'                 => '',
-
-                // css селектор для контейнера витрины
-                // (обязательный)
-                'container'           => '',
-
-                // css селектор для элемента витрины (относительно контейнера)
-                // (обязательный)
-                'element'             => '',
-
-                // ** дальнейшие css-селекторы указываются относительно element
-
-                // css селектор для ссылки на элемент !должен содержать конечный аттрибут href!
-                // (обязательный + должен быть обязательный атрибут, где хранится ссылка)
-                'element-link'        => '',
-
-                // css селектор для названия элемента
-                // (опционально)
-                'element-title'       => '',
-
-                // css селектор для описания элемента
-                // (опционально)
-                'element-description' => '',
-
-                // css селектор !должен содержать конечный аттрибут src! для картинки элемента
-                // (опционально)
-                'element-image'       => '',
-
-                // css селектор для даты элемента
-                // (опционально)
-                'element-date'        => '',
+                'element-date'  => 'pubDate',
             ],
 
             // настройка карточки элемента
@@ -161,14 +123,14 @@ class CORE_KhabarovskMd_Parser extends ParserCore implements ParserInterface
                 // css-селектор для контейнера карточки
                 // (можно несколько через запятую, если есть разные шаблоны новости)
                 // (обязательный)
-                'container'           => '',
+                'container'           => '#dle-content .b-article',
 
                 // ** дальнейшие css-селекторы указываются относительно container
 
                 // css-селектор для основного текста * - данные внутри (картинки, ссылки) парсятся автоматически
                 // (можно несколько через запятую, если есть разные шаблоны новости)
                 // (обязательный)
-                'element-text'        => '',
+                'element-text'        => '#videollink-advert-block',
 
                 // css-селектор даты создания новости
                 // (опционально)
@@ -191,7 +153,7 @@ class CORE_KhabarovskMd_Parser extends ParserCore implements ParserInterface
                 // игнорируемые css-селекторы (будут вырезаться из результата)
                 // (можно несколько через запятую)
                 // (опционально)
-                'ignore-selectors'    => '',
+                'ignore-selectors'    => 'p:nth-child(1), p:nth-child(2), p:nth-child(3) ',
 
                 // css-селекторы которые будут вставлятся в начало текста новости element-text (селекторы ищутся от корня, т.е. не зависят от container)
                 // (опционально)
