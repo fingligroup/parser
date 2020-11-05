9190c35c23c83c33f68ffde6c67abe67543d83d5
Roman Goncharenya <goncharenya@gmail.com>
2020-11-05 10:51:59 +0300

    part 3 approved by rmn

--- components/parser/news/CORE_OpennovRu_Parser.php
+++ components/parser/news/CORE_OpennovRu_Parser.php
@@ -17,20 +17,20 @@ namespace app\components\parser\news;
 use fingli\ParserCore\ParserCore;
 use app\components\parser\ParserInterface;
 
-// CORE_XXX_Parser -> необходимо заменить на актуальное название парсера (так как называется ваш файл)
+// part 3 approved by rmn
 class CORE_OpennovRu_Parser extends ParserCore implements ParserInterface
 {
     const USER_ID = 2;
     const FEED_ID = 2;
     // поддерживаемая версия ядра
     // (НЕ ИЗМЕНЯТЬ САМОСТОЯТЕЛЬНО!)
-    const FOR_CORE_VERSION = '1.0';
+    const FOR_CORE_VERSION = '1.7';
     // дебаг-режим (только для разработки) - выводит информацию о действиях парсера
     // 0 - отключен
     // 1 - включен
     // 2 - включен (очень подробный режим)
     // 3 - режим "зануда"
-    protected const DEBUG = 1;
+    protected const DEBUG = 0;
 
     public function __construct()
     {
@@ -38,25 +38,25 @@ class CORE_OpennovRu_Parser extends ParserCore implements ParserInterface
             // режимы работы парсера:
             // rss - RSS витрина
             // desktop - обычный сайт HTML
-            'mode'    => 'desktop',
+            'mode'       => 'desktop',
 
             // максимальное количество новостей, берушихся с витрины
             // ИСПОЛЬЗУЕТСЯ ТОЛЬКО В РЕЖИМЕ DEBUG
             // в остальных случаях жестко задается ядром
             //
             // не забывайте отключать лимит при сдаче парсера!
-                        'itemsLimit' => 1,
+            'itemsLimit' => 1,
 
             // настройки сайта
-            'site'    => [
+            'site'       => [
                 // протокол и домен
                 // (обязательный)
                 'url'         => 'https://opennov.ru',
 
                 // использовать юзер-агенты в http запросах.
                 // (опционально)
-                'user_agent'  => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/42.0',
-                                'user_agent'  => 'bot',
+                //                'user_agent'  => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/42.0',
+                'user_agent'  => 'bot',
 
                 // часовой пояс UTC.
                 // Чтобы определить часовой пояс, нужно зайти на https://time.is/Moscow и выбрать ближайший крупный город к которому относится сайт
@@ -83,42 +83,9 @@ class CORE_OpennovRu_Parser extends ParserCore implements ParserInterface
                 //                'pause'       => 0,
             ],
 
-            // настройки витрины (режим RSS)
-            'rss'     => [
-                // относительный URL где находится RSS
-                // (обязательный)
-                'url'                 => '',
-
-                // css селектор для элемента витрины (желательно от корня)
-                // (обязательный)
-                'element'             => 'rss > channel > item',
-
-                    // ** дальнейшие css-селекторы указываются относительно element
-
-                    // css селектор для названия элемента
-                    // (обязательный)
-                    'element-title'       => 'title',
-
-                    // css селектор для ссылки
-                    // (обязательный)
-                    'element-link'        => 'link',
-
-                    // css селектор для описания элемента
-                    // (опционально)
-                    'element-description' => 'description',
-
-                    // css селектор для картинки элемента
-                    // (опционально)
-                    'element-image'       => 'enclosure[url]',
-
-                    // css селектор для даты элемента
-                    // (опционально)
-                    'element-date'        => 'pubDate',
-            ],
-
             // настройки витрины (режим HTML)
             // !!! заполняется, только при отсутствии витрины RSS !!!
-            'list'    => [
+            'list'       => [
                 // URL где находится витрина
                 // (обязательный)
                 'url'                 => '/news',
@@ -127,66 +94,68 @@ class CORE_OpennovRu_Parser extends ParserCore implements ParserInterface
                 // (обязательный)
                 'container'           => '.flex-list__items',
 
-                    // css селектор для элемента витрины (относительно контейнера)
-                    // (обязательный)
-                    'element'             => '.teaser__header',
+                // css селектор для элемента витрины (относительно контейнера)
+                // (обязательный)
+                //                'element'             => '.teaser__header',
+                'element'             => '.flex-list__item article',
 
-                        // ** дальнейшие css-селекторы указываются относительно element
+                // ** дальнейшие css-селекторы указываются относительно element
 
-                        // css селектор для ссылки на элемент !должен содержать конечный аттрибут href!
-                        // (обязательный + должен быть обязательный атрибут, где хранится ссылка)
-                        'element-link'        => 'a.teaser__link [href]',
+                // css селектор для ссылки на элемент !должен содержать конечный аттрибут href!
+                // (обязательный + должен быть обязательный атрибут, где хранится ссылка)
+                'element-link'        => 'a.teaser__poster[href]',
 
-                        // css селектор для названия элемента
-                        // (опционально)
-                        'element-title'       => '.teaser__title',
+                // css селектор для названия элемента
+                // (опционально)
+                'element-title'       => '.teaser__title',
 
-                        // css селектор для описания элемента
-                        // (опционально)
-                        'element-description' => '.teaser__content',
+                // css селектор для описания элемента
+                // (опционально)
+                'element-description' => '.teaser__content',
 
-                        // css селектор !должен содержать конечный аттрибут src! для картинки элемента
-                        // (опционально)
-                        'element-image'       => '',
+                // css селектор !должен содержать конечный аттрибут src! для картинки элемента
+                // (опционально)
+                //                'element-image'       => '.teaser__poster img[src]',
 
-                        // css селектор для даты элемента
-                        // (опционально)
-                        'element-date'        => '',
+                // css селектор для даты элемента
+                // (опционально)
+                'element-date'        => '.teaser__date',
             ],
 
             // настройка карточки элемента
             // *** в CSS-селекторах можно указывать несколько селекторов через запятую (например, если сайт имеет несколько шаблонов карточки новости). Селекторы должны быть уникальны, иначе возможны коллизии
-            'element' => [
+            'element'    => [
 
                 // css-селектор для контейнера карточки
                 // (можно несколько через запятую, если есть разные шаблоны новости)
                 // (обязательный)
-                'container'           => '.article__container',
+                //                'container'           => '.article__container',
+                'container'           => 'article.node-news',
 
-                    // ** дальнейшие css-селекторы указываются относительно container
+                // ** дальнейшие css-селекторы указываются относительно container
 
-                    // css-селектор для основного текста * - данные внутри (картинки, ссылки) парсятся автоматически
-                    // (можно несколько через запятую, если есть разные шаблоны новости)
-                    // (обязательный)
-                    'element-text'        => '.article__typography',
+                // css-селектор для основного текста * - данные внутри (картинки, ссылки) парсятся автоматически
+                // (можно несколько через запятую, если есть разные шаблоны новости)
+                // (обязательный)
+                'element-text'        => '.article__typography',
 
-                    // css-селектор даты создания новости
-                    // (опционально)
-                    'element-date'        => 'a.article__date',
+                // css-селектор даты создания новости
+                // (опционально)
+                'element-date'        => 'a.article__date',
 
-                    // css селектор для описания элемента
-                    // (опционально)
-                    'element-description' => '',
+                // css селектор для описания элемента
+                // (опционально)
+                'element-description' => '',
 
-                    // css селектор для получения картинки
-                    // !должен содержать конечный аттрибут src! (например: img.main-image[src])
-                    // (опционально)
-                    'element-image'       => '',
+                // css селектор для получения картинки
+                // !должен содержать конечный аттрибут src! (например: img.main-image[src])
+                // (опционально)
+                'element-image'       => '.article__header .article__poster .image__image[style]',
 
-                    // css-селектор для цитаты
-                    // (если не заполнено, то по умолчанию берутся теги: blockquote и q)
-                    // (опционально)
-                    'element-quote'       => 'blockquote',
+                // css-селектор для цитаты
+                // (если не заполнено, то по умолчанию берутся теги: blockquote и q)
+                // (опционально)
+                'element-quote'       => 'blockquote',
 
                 // игнорируемые css-селекторы (будут вырезаться из результата)
                 // (можно несколько через запятую)
@@ -200,6 +169,29 @@ class CORE_OpennovRu_Parser extends ParserCore implements ParserInterface
         ];
 
         parent::__construct();
+    }
+
+    protected function getCardTextHtml(string $html)
+    : string {
+        // добавляем css-селекторы в начало и/или в конец текста
+        $html = $this->getHtmlWithInsertedSelectors($html);
+
+        // вырезаем игнорируемые теги
+        $html = $this->getHtmlWithoutIgnoredSelectors($html);
+
+        // подменяем цитаты
+        $html = $this->getHtmlWithSubstitutedQuotes($html);
+
+        $html = str_replace('<p></p>', '', $html);
+        $html = str_replace('<p><br></p>', '', $html);
+        $html = str_replace('<p>&nbsp;</p>', '', $html);
+        $html = str_replace('<p> </p>', '', $html);
+        $html = str_replace('<p> </p>', '', $html);
+
+        // оставляем только нужные теги
+        $html = $this->stripTags($html, $this->allowedTags);
+
+        return $html;
     }
 
     public static function run()
