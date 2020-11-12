ca940811f2fc32fcba8810a109cd335b728c2833
Roman Goncharenya <goncharenya@gmail.com>
2020-11-12 17:10:15 +0300

    4

--- components/parser/news/CORE_SpbvedomostiRu_Parser.php
+++ components/parser/news/CORE_SpbvedomostiRu_Parser.php
@@ -14,17 +14,19 @@
 
 namespace app\components\parser\news;
 
+use app\components\parser\NewsPostItem;
 use fingli\ParserCore\ParserCore;
 use app\components\parser\ParserInterface;
 
-// CORE_XXX_Parser -> необходимо заменить на актуальное название парсера (так как называется ваш файл)
+
+// part 4
 class CORE_SpbvedomostiRu_Parser extends ParserCore implements ParserInterface
 {
     const USER_ID = 2;
     const FEED_ID = 2;
     // поддерживаемая версия ядра
     // (НЕ ИЗМЕНЯТЬ САМОСТОЯТЕЛЬНО!)
-    const FOR_CORE_VERSION = '1.0';
+    const FOR_CORE_VERSION = '1.8';
     // дебаг-режим (только для разработки) - выводит информацию о действиях парсера
     // 0 - отключен
     // 1 - включен
@@ -94,31 +96,31 @@ class CORE_SpbvedomostiRu_Parser extends ParserCore implements ParserInterface
                 // (обязательный)
                 'container'           => '.content',
 
-                    // css селектор для элемента витрины (относительно контейнера)
-                    // (обязательный)
-                    'element'             => '.card',
+                // css селектор для элемента витрины (относительно контейнера)
+                // (обязательный)
+                'element'             => '.card',
 
-                        // ** дальнейшие css-селекторы указываются относительно element
+                // ** дальнейшие css-селекторы указываются относительно element
 
-                        // css селектор для ссылки на элемент !должен содержать конечный аттрибут href!
-                        // (обязательный + должен быть обязательный атрибут, где хранится ссылка)
-                        'element-link'        => '.card_link[data-href]',
+                // css селектор для ссылки на элемент !должен содержать конечный аттрибут href!
+                // (обязательный + должен быть обязательный атрибут, где хранится ссылка)
+                'element-link'        => '.card_link[data-href]',
 
-                        // css селектор для названия элемента
-                        // (опционально)
-                        'element-title'       => '.card h4',
+                // css селектор для названия элемента
+                // (опционально)
+                'element-title'       => '.card h4',
 
-                        // css селектор для описания элемента
-                        // (опционально)
-                        'element-description' => '.card p',
+                // css селектор для описания элемента
+                // (опционально)
+                'element-description' => '.card p',
 
-                        // css селектор !должен содержать конечный аттрибут src! для картинки элемента
-                        // (опционально)
-                        'element-image'       => '',
+                // css селектор !должен содержать конечный аттрибут src! для картинки элемента
+                // (опционально)
+                'element-image'       => '',
 
-                        // css селектор для даты элемента
-                        // (опционально)
-                        'element-date'        => '.card__date',
+                // css селектор для даты элемента
+                // (опционально)
+                'element-date'        => '.card__date',
             ],
 
             // настройка карточки элемента
@@ -128,37 +130,37 @@ class CORE_SpbvedomostiRu_Parser extends ParserCore implements ParserInterface
                 // css-селектор для контейнера карточки
                 // (можно несколько через запятую, если есть разные шаблоны новости)
                 // (обязательный)
-                'container'           => '.article',
+                'container'           => '.page-content',
 
-                    // ** дальнейшие css-селекторы указываются относительно container
+                // ** дальнейшие css-селекторы указываются относительно container
 
-                    // css-селектор для основного текста * - данные внутри (картинки, ссылки) парсятся автоматически
-                    // (можно несколько через запятую, если есть разные шаблоны новости)
-                    // (обязательный)
-                    'element-text'        => '.article',
+                // css-селектор для основного текста * - данные внутри (картинки, ссылки) парсятся автоматически
+                // (можно несколько через запятую, если есть разные шаблоны новости)
+                // (обязательный)
+                'element-text'        => '.article',
 
-                    // css-селектор даты создания новости
-                    // (опционально)
-                    'element-date'        => '',
+                // css-селектор даты создания новости
+                // (опционально)
+                'element-date'        => '',
 
-                    // css селектор для описания элемента
-                    // (опционально)
-                    'element-description' => '',
+                // css селектор для описания элемента
+                // (опционально)
+                'element-description' => '',
 
-                    // css селектор для получения картинки
-                    // !должен содержать конечный аттрибут src! (например: img.main-image[src])
-                    // (опционально)
-                    'element-image'       => '.article > img[src]',
+                // css селектор для получения картинки
+                // !должен содержать конечный аттрибут src! (например: img.main-image[src])
+                // (опционально)
+                'element-image'       => '.article > img[src]',
 
-                    // css-селектор для цитаты
-                    // (если не заполнено, то по умолчанию берутся теги: blockquote и q)
-                    // (опционально)
-                    'element-quote'       => '.article p b i',
+                // css-селектор для цитаты
+                // (если не заполнено, то по умолчанию берутся теги: blockquote и q)
+                // (опционально)
+                'element-quote'       => '.article p b i',
 
                 // игнорируемые css-селекторы (будут вырезаться из результата)
                 // (можно несколько через запятую)
                 // (опционально)
-                'ignore-selectors'    => '.inject, .article-info, .photo, .tags, .share, .similar, .comments, .mgbox, .partners__smi2',
+                'ignore-selectors'    => '.inject, .article-info, .photo, .tags, .share, .similar, .comments, .mgbox, .partners__smi2, div[style="display:none"]',
 
                 // css-селекторы которые будут вставлятся в начало текста новости element-text (селекторы ищутся от корня)
                 // (опционально)
@@ -176,6 +178,33 @@ class CORE_SpbvedomostiRu_Parser extends ParserCore implements ParserInterface
 
         $items = $Parser->getItems();
         $posts = $Parser->getCards(array_keys($items));
+
+
+        if (!empty($posts))
+        {
+            foreach ($posts as $post)
+            {
+                if (!empty($post->items))
+                {
+                    $postItemsNew = [];
+
+                    foreach ($post->items as $postItem)
+                    {
+                        if ($postItem->type == NewsPostItem::TYPE_TEXT)
+                        {
+                            if ($postItem->text == 'Загрузка...')
+                            {
+                                continue;
+                            }
+                        }
+
+                        $postItemsNew[] = $postItem;
+                    }
+
+                    $post->items = $postItemsNew;
+                }
+            }
+        }
 
         return $posts;
     }
