72636920ba6fac7d419bcde799a31993f343b053
Roman Goncharenya <goncharenya@gmail.com>
2020-11-13 22:25:31 +0300

    4

--- components/parser/news/CORE_ZelenogradRu_Parser.php
+++ components/parser/news/CORE_ZelenogradRu_Parser.php
@@ -17,14 +17,14 @@ namespace app\components\parser\news;
 use fingli\ParserCore\ParserCore;
 use app\components\parser\ParserInterface;
 
-// part 3 approved by rmn
+// part 4
 class CORE_ZelenogradRu_Parser extends ParserCore implements ParserInterface
 {
     const USER_ID = 2;
     const FEED_ID = 2;
     // поддерживаемая версия ядра
     // (НЕ ИЗМЕНЯТЬ САМОСТОЯТЕЛЬНО!)
-    const FOR_CORE_VERSION = '1.0';
+    const FOR_CORE_VERSION = '1.8';
     // дебаг-режим (только для разработки) - выводит информацию о действиях парсера
     // 0 - отключен
     // 1 - включен
@@ -87,33 +87,33 @@ class CORE_ZelenogradRu_Parser extends ParserCore implements ParserInterface
             'rss'     => [
                 // относительный URL где находится RSS
                 // (обязательный)
-                'url'                 => '/news/rss/all.xml',
+                'url'           => '/news/rss/all.xml',
 
                 // css селектор для элемента витрины (желательно от корня)
                 // (обязательный)
-                'element'             => 'rss > channel > item',
+                'element'       => 'rss > channel > item',
 
                 // ** дальнейшие css-селекторы указываются относительно element
 
                 // css селектор для названия элемента
                 // (обязательный)
-                'element-title'       => 'title',
+                'element-title' => 'title',
 
                 // css селектор для ссылки
                 // (обязательный)
-                'element-link'        => 'link',
+                'element-link'  => 'link',
 
                 // css селектор для описания элемента
                 // (опционально)
-                'element-description' => 'description',
+                //                'element-description' => 'description',
 
                 // css селектор для картинки элемента
                 // (опционально)
-                'element-image'       => '',
+                'element-image' => '',
 
                 // css селектор для даты элемента
                 // (опционально)
-                'element-date'        => 'pubDate',
+                'element-date'  => 'pubDate',
             ],
 
             // настройка карточки элемента
@@ -172,6 +172,33 @@ class CORE_ZelenogradRu_Parser extends ParserCore implements ParserInterface
 
         $items = $Parser->getItems();
         $posts = $Parser->getCards(array_keys($items));
+
+
+        // просто какой-то бред
+        //        if (!empty($posts))
+        //        {
+        //            foreach ($posts as $post)
+        //            {
+        //                $postItems = [];
+        //
+        //                if (!empty($post->items))
+        //                {
+        //                    foreach ($post->items as $postItem)
+        //                    {
+        //                        if (strpos($post->description, $postItem->text) !== false)
+        //                        {
+        //                            //                            echo '123;';
+        //                        }
+        //                        else
+        //                        {
+        //                            $postItems[] = $postItem;
+        //                        }
+        //                    }
+        //
+        //                    $post->items = $postItems;
+        //                }
+        //            }
+        //        }
 
         return $posts;
     }
