9ea11f31ccbbc24e63c47f46fc390cca6628809c
Roman Goncharenya <goncharenya@gmail.com>
2020-11-15 13:18:56 +0300

    4

--- components/parser/news/CORE_KvnewsRu_Parser.php
+++ components/parser/news/CORE_KvnewsRu_Parser.php
@@ -16,15 +16,17 @@ namespace app\components\parser\news;
 
 use fingli\ParserCore\ParserCore;
 use app\components\parser\ParserInterface;
+use Symfony\Component\DomCrawler\Crawler;
+use yii\base\Exception;
 
-// CORE_XXX_Parser -> необходимо заменить на актуальное название парсера (так как называется ваш файл)
+// part 4
 class CORE_KvnewsRu_Parser extends ParserCore implements ParserInterface
 {
     const USER_ID = 2;
     const FEED_ID = 2;
     // поддерживаемая версия ядра
     // (НЕ ИЗМЕНЯТЬ САМОСТОЯТЕЛЬНО!)
-    const FOR_CORE_VERSION = '1.0';
+    const FOR_CORE_VERSION = '1.12';
     // дебаг-режим (только для разработки) - выводит информацию о действиях парсера
     protected const DEBUG = 0;
 
@@ -38,7 +40,7 @@ class CORE_KvnewsRu_Parser extends ParserCore implements ParserInterface
 
             // максимальное количество новостей, берушихся с витрины
             // (опционально)
-                        'itemsLimit' => 1,
+            //            'itemsLimit' => 3,
 
             // настройки сайта
             'site'    => [
@@ -71,37 +73,40 @@ class CORE_KvnewsRu_Parser extends ParserCore implements ParserInterface
                 // формат даты в RSS
                 // (указывать только если он отличается от стандартного D, d M Y H:i:s O!)
                 //                'date_format_rss' => 'D, d M Y H:i:s O',
+
+                // кодировка сайта (если определяется некорректно)
+                //                'encoding'    => 'UTF-8'
             ],
 
             // настройки витрины (режим RSS)
             'rss'     => [
                 // относительный URL где находится RSS
                 // (обязательный)
-                'url'                 => '/structure/rss/all',
+                'url'           => '/structure/rss/all',
 
                 // css селектор для элемента витрины (желательно от корня)
                 // (обязательный)
-                'element'             => 'rss > channel > item',
+                'element'       => 'rss > channel > item',
 
                 // css селектор для названия элемента (относительно элемента)
                 // (обязательный)
-                'element-title'       => 'title',
+                'element-title' => 'title',
 
                 // css селектор для ссылки (относительно элемента)
                 // (обязательный)
-                'element-link'        => 'link',
+                'element-link'  => 'link',
 
                 // css селектор для описания элемента (относительно элемента)
                 // (заполняется только, если отсутствует в карточке)
-                'element-description' => 'description',
+                //                'element-description' => 'description',
 
                 // css селектор для картинки элемента (относительно элемента)
                 // (заполняется только, если отсутствует в карточке)
-                'element-image'       => 'enclosure[url]',
+                'element-image' => 'enclosure[url]',
 
                 // css селектор для даты элемента (относительно элемента)
                 // (заполняется только, если отсутствует в карточке)
-                'element-date'        => 'pubDate',
+                'element-date'  => 'pubDate',
             ],
 
             // настройка карточки элемента
@@ -111,7 +116,12 @@ class CORE_KvnewsRu_Parser extends ParserCore implements ParserInterface
                 // css-селектор для контейнера карточки
                 // (все дальнейшие пути строятся относительно этого контейнера)
                 // (обязательный)
-                'container'           => '.news-page:nth-child(1)',
+                //                'container'           => '.news-page:nth-child(1)',
+                //                'container'           => '.-grid-center-wr > .news-page',
+                //                'container'           => '.-margin-side:nth-child(2)  .news-page',
+                //                'container'           => '.-margin-side:nth-of-type(2)  .news-page',
+                //                'container'           => '.news-page:nth-child(2)',
+                'container'           => '.contain',
 
                 // css-селектор для основного текста
                 // (для заполнения модели NewsPostItem)
@@ -124,7 +134,7 @@ class CORE_KvnewsRu_Parser extends ParserCore implements ParserInterface
 
                 // css селектор для описания элемента (относительно элемента)
                 // (заполняется только, если отсутствует в витрине)
-                'element-description' => '',
+                'element-description' => '.news-page-content h3:first-child',
 
                 // css селектор для получения картинки
                 // !должен содержать конечный аттрибут src! (например: img.main-image[src])
@@ -139,11 +149,69 @@ class CORE_KvnewsRu_Parser extends ParserCore implements ParserInterface
                 // игнорируемые css-селекторы
                 // (можно через запятую)
                 // (опционально)
-                'ignore-selectors'    => '',
+                'ignore-selectors'    => '.news-page-content h3:first-child',
             ]
         ];
 
         parent::__construct();
+    }
+
+    // @bug: почему-то  текст оказался внутри комментария
+    // геттер элементов HTML
+    protected
+    function getElementsDataFromHtml(string $html, string $containerSelector, string $elementSelector, string $get = 'html'
+    )
+    : array {
+        $fullSelector = trim($containerSelector . ' ' . $elementSelector);
+
+        if (empty($fullSelector))
+        {
+            throw new Exception('Не установлен CSS-селектор!');
+        }
+
+        // решаем проблемы с кодировкой
+        if ($this->currentCharset != 'utf-8')
+        {
+            $html = str_replace('text/html; charset=' . $this->currentCharset, 'text/html; charset=utf-8', $html);
+            $html = str_replace('<meta charset="' . $this->currentCharset . '">', '<meta charset="utf-8">', $html);
+        }
+
+        $data = [];
+
+        $html = str_replace('<!--div class="ya-site-form ya-site-form_inited_no"', '<div class="ya-site-form ya-site-form_inited_no"', $html);
+
+        $Crawler   = new Crawler($html);
+        $attribute = $this->getAttrFromSelector($elementSelector);
+        $elements  = $Crawler->filter($fullSelector);
+
+        if ($elements)
+        {
+            $elements->each(function (Crawler $element, $i) use (&$data, $get, $attribute, $fullSelector) {
+                if (!empty($attribute))
+                {
+                    // если запрашивается style, то ищем ссылку
+                    if ($attribute == 'style')
+                    {
+                        $attrVal = $element->attr($attribute);
+                        $data[]  = $this->getUrlFromStyleAttr($attrVal);
+                    }
+                    else
+                    {
+                        $data[] = $element->attr($attribute);
+                    }
+                }
+                elseif ($get == 'html')
+                {
+                    $data[] = $element->outerHtml();
+                }
+                elseif ($get == 'text')
+                {
+                    $data[] = $element->text();
+                }
+            });
+        }
+
+        return $data;
     }
 
     public static function run()
