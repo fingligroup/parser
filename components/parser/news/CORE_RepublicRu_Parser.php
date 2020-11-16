d4453a54349b1558876cd036ab371776fde41cd9
Roman Goncharenya <goncharenya@gmail.com>
2020-11-16 10:41:09 +0300

    fix image

--- components/parser/news/CORE_RepublicRu_Parser.php
+++ components/parser/news/CORE_RepublicRu_Parser.php
@@ -114,7 +114,7 @@ class CORE_RepublicRu_Parser extends ParserCore implements ParserInterface
 
                 // css селектор !должен содержать конечный аттрибут src! для картинки элемента
                 // (опционально)
-                'element-image'       => '.image__src[src]',
+                //                'element-image'       => '.image__src[src]',
 
                 // css селектор для даты элемента
                 // (опционально)
@@ -148,7 +148,7 @@ class CORE_RepublicRu_Parser extends ParserCore implements ParserInterface
                 // css селектор для получения картинки
                 // !должен содержать конечный аттрибут src! (например: img.main-image[src])
                 // (опционально)
-                'element-image'       => '',
+                'element-image'       => '.post-heroimage__image[src]',
 
                 // css-селектор для цитаты
                 // (если не заполнено, то по умолчанию берутся теги: blockquote и q)
