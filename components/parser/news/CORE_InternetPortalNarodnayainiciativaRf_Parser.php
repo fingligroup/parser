6cee770d2d5eb957ea8414760c4d0625548a9ac4
Roman Goncharenya <goncharenya@gmail.com>
2020-11-16 16:05:26 +0300

    Точного времени нет на сайте. Парсер устанавливает время по умолчанию 12:00 (за счет смещения в UTC, время становится 9:00)

--- components/parser/news/CORE_InternetPortalNarodnayainiciativaRf_Parser.php
+++ components/parser/news/CORE_InternetPortalNarodnayainiciativaRf_Parser.php
@@ -24,7 +24,7 @@ class CORE_InternetPortalNarodnayainiciativaRf_Parser extends ParserCore impleme
     const FEED_ID = 2;
     // поддерживаемая версия ядра
     // (НЕ ИЗМЕНЯТЬ САМОСТОЯТЕЛЬНО!)
-    const FOR_CORE_VERSION = '1.13';
+    const FOR_CORE_VERSION = '1.14';
     // дебаг-режим (только для разработки) - выводит информацию о действиях парсера
     // 0 - отключен
     // 1 - включен
